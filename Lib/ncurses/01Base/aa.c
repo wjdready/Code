32478967683c6923c8d7382d0d9748c8241d2e8c
weijundong <wjdready@163.com>
2023-02-17 00:20:49 +0800

    Add ncurses

---
+++ Lib/ncurses/01Base/aa.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ncurses/ncurses.h>
+
+int main(int argc, char const *argv[])
+{
+    // init screen and sets up screen
+    initscr();
+
+    // print to screen
+    printw("Hello World");
+
+    // refreshes the screen
+    refresh();
+
+    // pause the screen output
+    getch();
+
+    // deallocates memory and ends ncurses
+    endwin();
+    
+    return 0;
+}
