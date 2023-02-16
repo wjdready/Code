495bad53b31b9c3c703040f5b90c6f46115c5640
weijundong <wjdready@163.com>
2023-02-16 19:39:00 +0800

    Add test_commandV2

---
+++ C_Cpp/通过attribute_section快速测试/test_commandV2.h
@@ -0,0 +1,37 @@
+#pragma once
+
+typedef int (*command_func_t)(int, char **);
+
+typedef struct
+{
+    int (*fun)(int, char **);
+    const char *name;
+    const char *desc;
+    char _tmp[8]; /* 32 位对齐 */
+} test_command_t;
+
+#define TEST_COMMAND_FUNC_DEFINE(f) int f(int argc, char **argv)
+
+#define SECTION __attribute((used, section("test_command_section")))
+#define EXPORT_TEST_COMMAND(f, n, d)       \
+    static test_command_t _##f SECTION = { \
+        .fun = f,                          \
+        .name = n,                         \
+        .desc = d,                         \
+    }
+
+#define TEST_FUN_ADD(f, n, d, ...)         \
+    static int _##f(int argc, char **argv) \
+    {                                      \
+        f(__VA_ARGS__);                    \
+        return 0;                          \
+    }                                      \
+    EXPORT_TEST_COMMAND(_##f, n, d)
+
+#define TEST_FUN_ADD_AWAJSON(f, n, d, pdev, ...) \
+    static int _##f(int argc, char **argv)       \
+    {                                            \
+        f(pdev, argc, argv);                     \
+        return 0;                                \
+    }                                            \
+    EXPORT_TEST_COMMAND(_##f, n, d)
