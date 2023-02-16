e64217f8bef060ca15eea902da1f86fff7538159
weijundong <wjdready@163.com>
2023-02-16 23:48:54 +0800

    Update test_command

--- C_Cpp/通过attribute_section快速测试/test_command.c
+++ C_Cpp/通过attribute_section快速测试/test_command.c
@@ -2,79 +2,95 @@
 #include <stdio.h>
 #include <string.h>
 
-/* 获取函数列表所在的内存区间 */
+#ifdef __ARMCC_VERSION /* ARM C Compiler */
+extern test_command_t test_command_section$$Base;
+extern test_command_t test_command_section$$Limit;
+test_command_t *test_command_section_begin = &(test_command_section$$Base);
+test_command_t *test_command_section_end = &(test_command_section$$Limit);
+#elif defined(__GNUC__)
 extern test_command_t __start_test_command_section;
 extern test_command_t __stop_test_command_section;
-static test_command_t *test_command_section_begin = &__start_test_command_section;
-static test_command_t *test_command_section_end = &__stop_test_command_section;
+test_command_t *test_command_section_begin = &__start_test_command_section;
+test_command_t *test_command_section_end = &__stop_test_command_section;
+#else
+#error "The platform is not supported"
+#endif
 
-#define foreach_command(item) \
-    for (test_command_t *item = test_command_section_begin; \
-    item != test_command_section_end; item++)
+#define foreach_command(item)                                                                                          \
+    for (test_command_t *item = test_command_section_begin; item != test_command_section_end; item++)
+
+/* 快速定义内部命令, 内部命令什么也不做只是用来显示和提供帮助 */
+#define TEST_FUN_ADD_INTERNAL(label, n, d)                                                                             \
+    static int _##label(int argc, char **argv)                                                                         \
+    {                                                                                                                  \
+        return 0;                                                                                                      \
+    }                                                                                                                  \
+    EXPORT_TEST_COMMAND(_##label, n, d)
 
 /* 添加内部命令 */
 TEST_FUN_ADD_INTERNAL(last_result, "r", "Get last command exec result");
 TEST_FUN_ADD_INTERNAL(all_command, "a", "Show all supported command");
 
-static int get_args(char command_string[], char const tokens[], int argc_max, char* argv[])
+/* 将给定字符串拆分成参数列表 */
+static int test_command_get_args(char *command_string, const char *tokens, int argc_max, char **argv)
 {
-	int argc = 0;
-	char* token = strtok(command_string, tokens);
+    int argc = 0;
+    char *token = strtok(command_string, tokens);
 
-	while(token != NULL)
-	{
-		argv[argc++] = token;
-		if( argc >= argc_max )
-			break;
+    while (token != NULL)
+    {
+        argv[argc++] = token;
+        if (argc >= argc_max)
+            break;
 
-		token = strtok(NULL, tokens);
-	}
+        token = strtok(NULL, tokens);
+    }
 
-	return argc;
+    return argc;
 }
 
-int test_command_start(void)
+/**
+ * @brief 处理给定的输入字符串
+ *
+ * 例如 echo "Hello"
+ *
+ * @param input_string 输入字符串
+ *
+ * @return int
+ */
+int test_command(char *input_string)
 {
-    char command[1024];
     char *argv[32] = {0};
-    int result = 0;
-    
-    while (1)
+    static int result = 0;
+
+    int argc = test_command_get_args(input_string, " \n", 31, argv);
+    if (argc == 0)
+        return -1;
+
+    if (strcmp(argv[0], "a") == 0 || strcmp(argv[0], "help") == 0)
     {
-        printf("\nsh# ");
-        gets(command);
+        foreach_command(entry) test_command_printf("    %-15s\t -- %s\n", entry->name, entry->desc);
 
-        int argc = get_args(command, " \n", 31, argv);
+        return 0;
+    }
 
-        if(argc == 0) continue;
+    /* 打印上一次命令执行结果 */
+    if (strcmp(argv[0], "r") == 0)
+    {
+        test_command_printf("%d\n", result);
+        return 0;
+    }
 
-        if (strcmp(argv[0], "a") == 0 || strcmp(argv[0], "help") == 0)
+    foreach_command(entry)
+    {
+        if (strcmp(entry->name, argv[0]) == 0)
         {
-            foreach_command(entry)
-            {
-                printf("    %-15s\t -- %s\n", entry->name, entry->desc);
-            }
-
-            continue;
-        }
-
-        /* 打印上一次命令执行结果 */
-        if (strcmp(argv[0], "r") == 0)
-        {
-            printf("%d\n", result);
-            continue;
-        }
-
-        foreach_command(entry)
-        {
-            if (strcmp(entry->name, command) == 0)
-            {
-                result = entry->fun(argc, argv);
-                break;
-            }
+            result = entry->fun(argc, argv);
+            return result;
         }
     }
 
-    return 0;
-}
+    test_command_printf("Command %s not found\n", argv[0]);
 
+    return -1;
+}
